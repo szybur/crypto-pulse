43d6583117b464e201c7e1e1e7974cb37d8faef1
Emilian Szyburski <szybur@tlen.pl>
2026-04-13 18:39:24 +0200

    packages renaming

--- src/main/kotlin/com/pulse/crypto/Application.kt
+++ src/main/kotlin/com/pulse/crypto/Application.kt
@@ -1,9 +1,10 @@
-package pl.szybur
+package com.pulse.crypto
 
 import io.ktor.server.application.*
+import io.ktor.server.netty.EngineMain
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    EngineMain.main(args)
 }
 
 fun Application.module() {
--- src/main/kotlin/com/pulse/crypto/Routing.kt
+++ src/main/kotlin/com/pulse/crypto/Routing.kt
@@ -1,4 +1,4 @@
-package pl.szybur
+package com.pulse.crypto
 
 import io.ktor.server.application.*
 import io.ktor.server.response.*
